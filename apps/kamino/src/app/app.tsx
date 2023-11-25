965e1c1b54e321e89739c288630362e98202c0a5
Leon Menzies <leon.menzies@hotmail.com>
2023-11-25 14:01:38 -0800

    first commit

---
+++ apps/kamino/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Button } from '@musclemotion/components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <Button />
+    </StyledApp>
+  );
+}
+
+export default App;
