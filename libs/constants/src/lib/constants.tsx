58c0076ba00b5c0c9d4b41d8773aaf56e23226c5
Leon Menzies <leon.menzies@hotmail.com>
2024-01-29 19:15:32 -0800

    Updated README

--- libs/constants/src/lib/constants.tsx
+++ libs/constants/src/lib/constants.tsx
@@ -1,2 +1,2 @@
 export const URL_BASE = 'http://localhost:3000/api';
-export const AWS_PRODUCT_IMAGES_BASE = 'http://localhost:3001/product-images';
+export const AWS_PRODUCT_IMAGES_BASE = 'http://localhost:6000/product-images';
